b5b7c76a8525735f1cc1f33609d8d1d7053a46af
Nalini Chervela <nalini_chervela@dell.com>
2018-09-10 14:42:37 -0700

    feat : logrotate enhancements
    run logrotate utility as part of hourly cron job.
    
    Signed-off-by: Nalini Chervela <nalini_chervela@dell.com>

--- inst-hooks/102-logrotate-hourly.sh
+++ inst-hooks/102-logrotate-hourly.sh
@@ -3,6 +3,7 @@
 # This postinst script enables logrotate utility to be run hourly, instead 
 # of the daily default
 
+test -x /etc/cron.daily/logrotate || exit 0
 mv /etc/cron.daily/logrotate /etc/cron.hourly/
 chmod 777 /etc/cron.hourly/logrotate
 
